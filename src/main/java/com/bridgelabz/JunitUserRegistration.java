4c6f5c0c064ab919feab1f455c7bb1ec04854e41
kajal <tiwarikajal1306@gmail.com>
2020-06-19 09:43:19 +0530

    ADDED Test for valid email

--- src/main/java/com/bridgelabz/JunitUserRegistration.java
+++ src/main/java/com/bridgelabz/JunitUserRegistration.java
@@ -5,4 +5,10 @@ public class JunitUserRegistration {
     public boolean validFirstName(String firstName) {
         return firstName.matches("^[A-Z]{1}[a-z]{2,}");
     }
+
+
+    public boolean validateEmail(String emailid) {
+     return emailid.matches ("^[a-zA-Z0-9]+(([._+-]?)[a-zA-Z0-9]+)*@[a-zA-Z0-9]+.[a-zA-Z]{2,4}([.][a-zA-Z]{2,3})*$");
+
+    }
 }
--- src/test/java/com/bridgelabz/UserRegistration.java
+++ src/test/java/com/bridgelabz/UserRegistration.java
@@ -2,13 +2,25 @@ package com.bridgelabz;
 
 import org.junit.Assert;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 
+import java.util.Arrays;
+import java.util.Collection;
+
+@RunWith(Parameterized.class)
 public class UserRegistration {
 
        JunitUserRegistration junitUserRegistration = new JunitUserRegistration();
         boolean result;
 
+    private String keys;
+    private boolean values;
 
+    public UserRegistration(String keys, boolean values) {
+        this.keys = keys;
+        this.values = values;
+    }
     @Test
     public void firstNameValidation_True_ifFirstNameProper() {
         result = junitUserRegistration.validFirstName("Kajal");
@@ -44,5 +56,32 @@ public class UserRegistration {
         result = junitUserRegistration.validFirstName("tiwari");
         Assert.assertTrue(result);
     }
+    @Parameterized.Parameters
+    public static Collection<Object[]> testForEmail(){
+        Object[][] testForEmails = new Object[][]{
+                {"abc@yahoo.com", true},
+                {"abc-100@yahoo.com", true},
+                {"abc.100@yahoo.com", true},
+                {"abc111@abc.com", true},
+                {"abc-100@abc.net", true},
+                {"abc.100@abc.com.net", true},
+                {"abc@1.com", true},
+                {"abc@gmail.com.com", true},
+                {"abc+100@gmail.com", true},
+                {".abc@gmail.com", false},
+                {"alice.@example.c", false},
+                {"alice@example.dd..com", false},
+                {"alice:;@example.com", false},
+                {"?alice.@example.com", false}
+        };
+        return Arrays.asList(testForEmails);
+    }
+
+    @Test
+    public void validateEmail() {
+        boolean result = junitUserRegistration.validateEmail(keys);
+        Assert.assertEquals(result,values);
+    }
+
 
     }
